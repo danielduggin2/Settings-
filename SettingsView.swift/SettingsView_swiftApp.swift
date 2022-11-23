3fe9e34a89f84ce318dc1d8c6092faab5325b5ce
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2022-11-23 14:13:54 -0600

    Initial Commit

---
+++ SettingsView.swift/SettingsView_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsView_swiftApp.swift
+//  SettingsView.swift
+//
+//  Created by Daniel Wade Duggin on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SettingsView_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
