c12b8d4cf23d5d7bf3b54dd61f8daba395301031
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2022-11-23 16:55:05 -0600

    Original file. Need to fix dark mode selection and actual dark mode view.

---
+++ Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  SettingsView.swift
+//
+//  Created by Daniel Wade Duggin on 11/23/22.
+//
+
+import Foundation
+
+//this uniquely identifies each link. Basically make it static
+enum Constants{
+    static let instagram = "https://www.instagram.com/danielduggin"
+    static let email = "mailto:campuscrowdcheck@gmail.com"
+    static let phone = "tel:7316130660"
+    
+}
--- SettingsView.swift/ContentView.swift
+++ SettingsView.swift/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form{
+                Section(header: Text("Display"),
+                        footer: Text("System settings will override dark mode if toggled on. This will then use the device theme.")){
+                    Toggle(isOn: .constant(false),
+                           label: {
+                        Text("Dark Mode")
+                    })
+                    
+                    Toggle(isOn: .constant(false),
+                           label: {
+                        Text("Use System Settings")
+                    })
+                }
+                Section{
+                    Link(destination: URL(string: Constants.instagram)!,
+                            label: {
+                                Label("Follow us on instagram", systemImage: "link")
+                    })
+                   
+                    Link("Contact us via email",
+                         destination: URL(string: Constants.email)!)
+                    
+                    Link("Contact us via phone",
+                         destination: URL(string: Constants.phone)!)
+                   
+                }
+                .foregroundColor(.black)
+                .font(.system(size: 16, weight: .semibold))
+            }
+            .navigationTitle("Settings")
         }
-        .padding()
     }
 }
 
